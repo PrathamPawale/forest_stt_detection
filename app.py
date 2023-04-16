0da25788df1709eaaa6a3bd7dd95be37587f035b
Judieian <90787593+PrathamPawale@users.noreply.github.com>
2023-04-16 09:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,8 @@ if uploaded_file is not None:
     output_mask = conv_float_int(combine_image(preds_t, row_num, col_num, original_width, original_height, remove_ghost=False)[:,:,0])
     st.markdown("** The Predicted Mask is **: ")
     plt.imshow(output_mask)
-    st.pyplot(output_mask)
+    #st.pyplot(output_mask)
+    st.image(output_mask)
     #plt.imshow(output_mask)
    
     st.sidebar.markdown("** CO2 Emission Calculator **")
